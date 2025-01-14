f5543f6bd1c1e73c5c61a32740af3dd127e64fdf
YD620 <noobaf850@gmail.com>
2025-01-14 12:25:11 -0800

    Finished

---
+++ packages/react-frontend/src/main.jsx
@@ -0,0 +1,13 @@
+// src/main.jsx
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import MyApp from "./MyApp";
+import "./main.css";
+
+const container = document.getElementById("root");
+
+// Create a root
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render:
+root.render(<MyApp />);
