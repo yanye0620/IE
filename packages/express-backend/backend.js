3d3c1806b393781bea09119397e34a6a312b5720
YD620 <noobaf850@gmail.com>
2025-01-27 09:24:35 -0800

    updating

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -52,6 +53,7 @@ const addUser = (user) => {
   return user;
 };
 
+app.use(cors());
 app.use(express.json());
 
 /*app.get("/users", (req, res) => {
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,5 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -15,6 +15,40 @@ function MyApp() {
   }
   function updateList(person) {
     setCharacters([...characters, person]);
+  }
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(person)
+    });
+  
+    return promise;
+  }
+  
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
