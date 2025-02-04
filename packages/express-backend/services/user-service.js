2b42e3f9761611efde56dd5812c6bdbfc2886499
YD620 <noobaf850@gmail.com>
2025-02-04 12:24:15 -0800

    Finished

--- packages/express-backend/services/user-service.js
+++ packages/express-backend/services/user-service.js
@@ -1,14 +1,6 @@
 import mongoose from "mongoose";
 import userModel from "../models/user.js";
 
-mongoose.set("debug", true);
-
-mongoose
-.connect("mongodb://localhost:27017/users", {
-useNewUrlParser: true,
-useUnifiedTopology: true,
-})
-.catch((error) => console.log(error));
 
 function getUsers(name, job) {
     let promise;
